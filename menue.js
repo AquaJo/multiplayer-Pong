2be03d367058ea40433e10d0619bb2ab403d5d4f
AquaJo <84229101+AquaJo@users.noreply.github.com>
2022-04-25 11:30:46 +0200

    Add files via upload

--- menue.js
+++ menue.js
@@ -25,10 +25,8 @@ function mainMenue() {
       width / 4.7,
       (height / 3.6) + height / 5
     );
-    if(!isMobile) {
     inputOnceJ();
     inpJ.show();
-    }
     try {
       inpJ.position(width / 4.7 + width / 5, height / 2.35);
     } catch (error) {}
