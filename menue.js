39a5fd9e7400f9edf52e3c3d8f4457bd921eae94
AquaJo <84229101+AquaJo@users.noreply.github.com>
2022-04-25 09:36:45 +0200

    Update menue.js

--- menue.js
+++ menue.js
@@ -25,8 +25,10 @@ function mainMenue() {
       width / 4.7,
       (height / 3.6) + height / 5
     );
+    if(!isMobile) {
     inputOnceJ();
     inpJ.show();
+    }
     try {
       inpJ.position(width / 4.7 + width / 5, height / 2.35);
     } catch (error) {}
