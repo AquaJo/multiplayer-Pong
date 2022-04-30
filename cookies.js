35a3fde73ff1450b0caab04d6623e168d80f8bc6
AquaJo <84229101+AquaJo@users.noreply.github.com>
2022-04-30 18:38:35 +0200

    cookies set / get function added

--- cookies.js
+++ cookies.js
@@ -1,9 +1,29 @@
-function createCookie(name,value,days,path) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
+function createCookie(name, value, days) { //"," for simple arrays
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    var expires = "; expires=" + date.toGMTString();
+  } else var expires = "";
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/"+path;
+    if (c.indexOf(name) == 0) {
+      let res = c.substring(name.length, c.length);
+      if (res.includes(",")) {
+        return res.split(",");
+      } else {
+        return c.substring(name.length, c.length);
+      }
+    }
+  }
+  return "";
 }
